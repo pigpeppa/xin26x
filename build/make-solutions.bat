8f214230dbb6a062e1e6a8f338500434a33efc9e
pig.peppa <pig.peppa@qq.com>
2020-08-18 11:14:42 +0800

    minor change

--- build/make-solutions.bat
+++ build/make-solutions.bat
@@ -3,4 +3,4 @@
 :: run this batch file to create a Visual Studio solution file for this project.
 :: See the cmake documentation for other generator targets
 ::
-cmake -G "Visual Studio 12 Win64" ..\ && cmake-gui ..\
+cmake -G "Visual Studio 12 Win64" ..\
