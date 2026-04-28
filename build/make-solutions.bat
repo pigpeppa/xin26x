b0e21ea4bd0da18ade1f8780bab9de34319c0c96
pig.george <pig.george@qq.com>
2026-04-28 09:50:10 +0800

    update obsoleted bat.

--- build/make-solutions.bat
+++ build/make-solutions.bat
@@ -3,4 +3,4 @@
 :: run this batch file to create a Visual Studio solution file for this project.
 :: See the cmake documentation for other generator targets
 ::
-cmake -G "Visual Studio 12 Win64" ..\
+cmake -G "Visual Studio 17 2022" ..\ -DH265=ON -DH266=ON -DAV1=ON
