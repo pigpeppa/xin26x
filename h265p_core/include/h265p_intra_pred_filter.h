1a48492b7ea4dfc981697b1da5389faa17b8c570
pig.george <pig.george@qq.com>
2026-06-10 14:22:04 +0800

    add gpl3 declaration.

--- h265p_core/include/h265p_intra_pred_filter.h
+++ h265p_core/include/h265p_intra_pred_filter.h
@@ -1,3 +1,23 @@
+/***************************************************************************//**
+ *
+ * @file          h265p_intra_pred_filter.h
+ * @brief         This file declares av1 intra prediction filter subroutines and weights.
+ *
+ * @authors       Chao Zhou
+ *
+ * Xin26x Video Codec Library
+ *
+ * Copyright (C) 2020-2026 Chao Zhou <czhou2@qq.com>
+ *
+ * This file is part of Xin26x.
+ *
+ * Licensed under the GNU General Public License, Version 3 or later
+ * (GPL-3.0-or-later). See the LICENSE file for details.
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *******************************************************************************/
+
 #ifndef _h265p_intra_pred_filter_h_
 #define _h265p_intra_pred_filter_h_
 
